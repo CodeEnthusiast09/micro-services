868a98d99e46fa475462dc5b6f888f40afce351c
O'Brien <obrienadedapo15@gmail.com>
2025-07-04 13:15:42 +0100

    another commit

--- apps/uber-service-stdrd/src/app.service.ts
+++ apps/uber-service-stdrd/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! I am learning microservices with NEstJs';
+    return 'Hello World! I am learning microservices with NestJs';
   }
 }
